2ba51d09b6e537a276fa4af34fc5f4c1ac269130
root <root@DESKTOP-KO9QVBL.localdomain>
2017-07-25 10:55:47 +0800

    init

---
+++ src/Demo1/t1/t1.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Demo1\t1;
+
+class t1
+{
+    public static function info()
+    {
+        echo 't1<br>';
+    }
+}
---
+++ src/Demo1/t2/t2.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Demo1\t2;
+
+class t2
+{
+    public static function info()
+    {
+        echo 't2<br>';
+    }
+}
